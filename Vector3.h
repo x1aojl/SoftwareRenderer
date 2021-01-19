a9dcf082133b9660569f9603ae177ec36b499300
x1aojl <x1aojl@icloud.com>
2021-01-19 14:18:07 +0800

    Update Vector3.h

--- Vector3.h
+++ Vector3.h
@@ -4,6 +4,8 @@
 
 #pragma once
 
+# include <cmath>
+
 class Vector3
 {
 public:
@@ -12,12 +14,31 @@ public:
 
 public:
 	Vector3& operator =(const Vector3 &rhs);
+	bool operator ==(const Vector3 &rhs) const;
+	bool operator !=(const Vector3 &rhs) const;
+	Vector3 operator -() const;
+	Vector3 operator +(const Vector3 &rhs) const;
+	Vector3 operator -(const Vector3 &rhs) const;
+	Vector3 operator *(const float &rhs) const;
+	Vector3 operator /(const float &rhs) const;
+	Vector3& operator +=(const Vector3 &rhs);
+	Vector3& operator -=(const Vector3 &rhs);
+	Vector3& operator *=(const float &rhs);
+	Vector3& operator /=(const float &rhs);
 
 public:
+	float Dot(const Vector3 &rhs) const;
+	Vector3& Cross(const Vector3 &rhs) const;
+	Vector3& Normalize();
 	void Zero();
 	
+public:
+	static const Vector3 zero;
+
 public:
 	float x;
 	float y;
 	float z;
 };
+
+const Vector3 zero = Vector3(0, 0, 0);
