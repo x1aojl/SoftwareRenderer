bbca2266c219b89f6ad9385375ef30d1c1636653
x1aojl <x1aojl@icloud.com>
2021-01-19 14:17:38 +0800

    Update Device.cpp

--- Device.cpp
+++ Device.cpp
@@ -19,62 +19,98 @@ void Device::Clear()
 		depthBuffer[i] = FLT_MAX;
 }
 
-// 光栅化阶段
-void Device::Rasterization(std::vector<Vector3> &mesh)
+// 几何阶段
+void Device::GeometryStage(Vector4 &vertex1, Vector4 &vertex2, Vector4 &vertex3)
 {
-	for (int i = 0; i < mesh.size(); i += 3)
+	std::vector<Vector4> vertices = std::vector<Vector4> { vertex1, vertex2, vertex3 };
+	for (auto &vertex : vertices)
 	{
-		std::vector<Vector3> fragments;
-		TriangleTraversal(mesh[0], mesh[1], mesh[2], fragments);
-		PerFragmentOperations(fragments);
+		// 模型视图变换
+		Vector3 meshRotation;
+		Matrix4x4 rotation = Matrix4x4::Translation(meshRotation);
+
+		Vector3 meshPosition;
+		Matrix4x4 translation = Matrix4x4::Translation(meshPosition);
+
+		Matrix4x4 world = rotation * translation;
+
+		Vector3 CameraPosition = Vector3(0, 0, 10);
+		Vector3 CameraForward = Vector3(0, 0, -10);
+		Vector3 CameraUp = Vector3(0, 1, 0);
+		Matrix4x4 view = Matrix4x4::LookAtLH(CameraPosition, CameraForward, CameraUp);
+		Matrix4x4 projection = Matrix4x4::PerspectiveFovLH(0.78f, (float)width / height, 0.01f, 1.0f);
+
+		Matrix4x4 mvp = world * view * projection;
+
+		if (Clipping(vertex))
+			return;
+
+		ScreenMapping(vertex);
 	}
 }
 
-// 逐片元操作（模板测试，深度测试，颜色混合）
-void Device::PerFragmentOperations(const std::vector<Vector3> &fragments)
+// CVV裁剪
+bool Device::Clipping(const Vector4 &vertex)
 {
-	for (auto fragment : fragments)
+	float w = vertex.w;
+	if (vertex.z < 0. || vertex.z > w) return true;
+	if (vertex.x < -w || vertex.x > w) return true;
+	if (vertex.y < -w || vertex.y > w) return true;
+	return false;
+}
+
+// 屏幕映射
+void Device::ScreenMapping(Vector4 &vertex)
+{
+	float rhw = 1.0f / vertex.w;
+	vertex.x = 0.5f * width * (vertex.x * rhw + 1.0f);
+	vertex.y = 0.5f * height * (1.0f - vertex.y * rhw);
+	vertex.z = vertex.z * rhw;
+	vertex.w = 1.0f;
+}
+
+// 光栅化阶段
+void Device::RasterizerStage(Vector4 &vertex1, Vector4 &vertex2, Vector4 &vertex3)
+{
+	std::vector<Vector3> vertices = TriangleSetup(vertex1, vertex2, vertex3);
+	std::vector<Vector3> fragments = TriangleTraversal(vertices[0], vertices[1], vertices[2]);
+	PerFragmentOperations(fragments);
+}
+
+// 三角形设置
+std::vector<Vector3> Device::TriangleSetup(const Vector4 &vertex1, const Vector4 &vertex2, const Vector4 &vertex3)
+{
+	return std::vector<Vector3>
 	{
-		int index = fragment.x + fragment.y * width;
-		if (fragment.x < 0 || fragment.x >= width ||
-			fragment.y < 0 || fragment.y >= height)
-			continue;
-
-		// 深度测试
-		if (depthBuffer[index] < fragment.z)
-			continue;
-
-		// 更新深度缓冲
-		depthBuffer[index] >= fragment.z;
-
-		// 更新颜色缓冲
-		frameBuffer[index] = Vector4(0, 1, 1, 1);
-	}
+		Vector3(vertex1.x, vertex1.y, vertex1.z),
+		Vector3(vertex2.x, vertex2.y, vertex2.z),
+		Vector3(vertex3.x, vertex3.y, vertex3.z)
+	};
 }
 
 // 三角形遍历
-void Device::TriangleTraversal(Vector3 &v1, Vector3 &v2, Vector3 &v3, std::vector<Vector3> &fragments)
+//    v3
+// 	  -
+// 	  - -
+// vm -   - v2
+// 	  - -
+//    -
+//    v1
+// 将v1、v2、v3三个点按纵坐标y升序排列
+// 分别扫描v1-v2-vm和v2-vm-v3两个三角形
+std::vector<Vector3> Device::TriangleTraversal(const Vector3 &vertex1, const Vector3 &vertex2, const Vector3 &vertex3)
 {
-	// 扫描线
-	//    v3
-	// 	  -
-	// 	  - -
-	// vm -   - v2
-	// 	  - -
-	//    -
-	//    v1
-	// 将v1、v2、v3三个点按纵坐标y升序排列
-	// 分别扫描v1-v2-vm和v2-vm-v3两个三角形
+	std::vector<Vector3> fragments;
 
 	// 排序 v1.y <= v2.y <= v3.y
-	if (v1.y > v2.y) std::swap(v1, v2);
-	if (v2.y > v3.y) std::swap(v2, v3);
-	if (v1.y > v3.y) std::swap(v1, v3);
+	if (vertex1.y > vertex2.y) std::swap(vertex1, vertex2);
+	if (vertex2.y > vertex3.y) std::swap(vertex2, vertex3);
+	if (vertex1.y > vertex3.y) std::swap(vertex1, vertex3);
 
 	// 屏幕坐标为整数，将v1、v2、v3的坐标化整
-	int x1 = (int)v1.x, y1 = (int)v1.y, z1 = (int)v1.z;
-	int x2 = (int)v2.x, y2 = (int)v2.y, z2 = (int)v2.z;
-	int x3 = (int)v3.x, y3 = (int)v3.y, z3 = (int)v3.z;
+	int x1 = (int)vertex1.x, y1 = (int)vertex1.y, z1 = (int)vertex1.z;
+	int x2 = (int)vertex2.x, y2 = (int)vertex2.y, z2 = (int)vertex2.z;
+	int x3 = (int)vertex3.x, y3 = (int)vertex3.y, z3 = (int)vertex3.z;
 
 	// v1、v2、v3两两纵坐标差异值，加1防止除零
 	int diffY_12 = y2 - y1 + 1;
@@ -136,4 +172,28 @@ void Device::TriangleTraversal(Vector3 &v1, Vector3 &v2, Vector3 &v3, std::vecto
 		}
 	}
 #pragma endregion
+
+	return fragments;
+}
+
+// 逐片元操作（模板测试，深度测试，颜色混合）
+void Device::PerFragmentOperations(const std::vector<Vector3> &fragments)
+{
+	for (auto fragment : fragments)
+	{
+		int index = fragment.x + fragment.y * width;
+		if (fragment.x < 0 || fragment.x >= width ||
+			fragment.y < 0 || fragment.y >= height)
+			continue;
+
+		// 深度测试
+		if (depthBuffer[index] < fragment.z)
+			continue;
+
+		// 更新深度缓冲
+		depthBuffer[index] >= fragment.z;
+
+		// 更新颜色缓冲
+		frameBuffer[index] = Vector4(0, 1, 1, 1);
+	}
 }
